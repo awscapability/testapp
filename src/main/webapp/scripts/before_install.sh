7df3d85fbd9f07489a28b97792566d2583bf9722
SXP4516 <SXP4516@IRVSXP4516D.us.tgr.net>
2018-05-31 00:27:13 -0700

    3rd Commit

---
+++ src/main/webapp/scripts/before_install.sh
@@ -0,0 +1,9 @@
+sudo yum update
+sudo yum install ruby
+sudo yum install wget
+cd /home/ec2-user
+wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+cd /var/liv/tomcat/webapps
+cp bundle.tar PZDemoApp.war
